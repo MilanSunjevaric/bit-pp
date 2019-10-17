f81f80215d48c1ed56ad703d7603f5328c175246
BIT Student <classroom@bgit.rs>
2019-10-17 17:59:24 +0200

    Loops

---
+++ loop/while.js
@@ -0,0 +1,23 @@
+/*var counter = 0;
+
+
+while (counter < 100) {
+
+    counter++;
+    console.log(counter);
+}*/
+
+
+
+
+
+var counter = 2;
+var sum = 0;
+
+while (counter <= 100) {
+
+    sum = sum + counter;
+    counter + 2;
+}
+
+console.log(sum);

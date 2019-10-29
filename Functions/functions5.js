f42c49e3ff1cf8054d7a7f33cbdfc37ccc24dcfb
BIT Student <classroom@bgit.rs>
2019-10-29 18:04:46 +0100

    predefined

--- Functions/functions5.js
+++ Functions/functions5.js
@@ -190,4 +190,72 @@ Don't display two of the same numbers at the same time. Display the number
 
 'use strict';
 
-function display 
+function display(n) {
+    var a = '';
+    for (var i = 1; i <= n; i++) {
+        for (var j = 1; j <= n; j++) {
+            if (i !== j) {
+                a += '(' + i + " . " + j + ')' + "\n"
+            }
+        }
+    } return a;
+}
+console.log(display(7));
+
+
+/* Zadatak 10. Write a program that checks if the entered number is a prime number (i.e. divisible only by 1 and by itself).
+Input:  17    | 15
+Output: true  | false
+ */
+
+'use strict'
+function check(number) {
+
+    for (var i = 2; i < number; i++) {
+        if (number % i === 0) {
+
+            var c = "false";
+        }
+        else if (number % i !== 0) {
+            c = "true";
+        }
+    }
+    return c;
+}
+
+console.log(check(17));
+
+/* Zadatak 11. Check if a given string is a palindrome (spaces are ignored).
+Input:  eye  | Geek  | a nut for a jar of tuna
+Output: true | false | true
+ */
+'use strict'
+function palindrome(string) {
+    var second = "";
+
+    for (var j = string.length - 1; j >= 0; j--) {
+        second += string[j];
+    }
+
+    return string === second;
+}
+console.log(palindrome('anavolimilovana'));
+
+
+/* Zadatak 12. Write a program that calculates the greatest common divisor of two integers. Note: The greatest common divisor of two non-zero integers is the greatest positive number that divides both numbers with no remainder.
+Input:  192 42 | 81 9
+Output: 6      | 9
+ */
+
+function divisor(numOne, numTwo) {
+    var array = [];
+
+    for (var i = 1; i < numOne; i++) {
+        if (numOne % i === 0 && numTwo % i === 0) {
+            array[array.length] = i;
+            var c = array[array.length - 1];
+        }
+    }
+    return c;
+}
+console.log(divisor(192, 42));
